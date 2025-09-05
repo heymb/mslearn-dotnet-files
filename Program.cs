f43b4462c48ba607e94cb73ecbff4f2e514f4b13
Michael Baldwin <mtbaldwin10@gmail.com>
2025-09-04 22:18:24 -0400

    create directory and write to file (empty for now)

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), string.Empty);
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -16,7 +16,6 @@ IEnumerable<string> FindFiles(string folderName)
     foreach (var file in foundFiles)
     {
         var extension = Path.GetExtension(file);
-        // The file name will contain the full path, so only check the end of it
         if (extension == ".json")
         {
             salesFiles.Add(file);
